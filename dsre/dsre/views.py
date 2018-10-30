8e43192fa6b27958a9fdae66a782b2045cb573b6
William Giokas <1007380@gmail.com>
2018-10-30 19:27:57 +0100

    Debug

--- dsre/dsre/views.py
+++ dsre/dsre/views.py
@@ -6,8 +6,8 @@ log = logging.getLogger(__name__)
 def index(request):
     if request.method == "GET":
         if request.META['HTTP_ACCEPT'] == "application/json":
-            logging.debug("returning a json object")
+            log.debug("returning a json object")
             return HttpResponse('{"message": "Good morning"}')
         else:
-            logging.debug("returning an html object")
+            log.debug("returning an html object")
             return HttpResponse("<p>Hello, World</p>")
