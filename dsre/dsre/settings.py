ec30f2af49004ba77d69036c0314ce036008ae94
William Giokas <1007380@gmail.com>
2018-11-15 10:25:48 -0800

    part two plangrid

--- dsre/dsre/settings.py
+++ dsre/dsre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q2x17zqo*wr^q5)-7tcs*u8+@c5bwj-5b_pv5zd1!7c@wrd7vo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
--- dsre/dsre/views.py
+++ dsre/dsre/views.py
@@ -3,11 +3,19 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def index(request):
+def index(request, name='john'):
+    lang = request.GET.get('lang', 'en')
+    jsonmsg = '{"message": "Hola %s!"}' % name
     if request.method == "GET":
         if request.META.get('HTTP_ACCEPT', None) == "application/json":
             log.debug("returning a json object")
-            return HttpResponse('{"message": "Good morning"}')
+            if lang == "es":
+                return HttpResponse(jsonmsg)
+            else:
+                return HttpResponse('{"message": "Good morning"}')
         else:
             log.debug("returning an html object")
-            return HttpResponse("<p>Hello, World</p>")
+            if lang == "es":
+                return HttpResponse(f"<p>Hola, {name}</p>")
+            else:
+                return HttpResponse("<p>Hello, World</p>")
